449dd0325a9bc8f8fbbc68d6d9dbce284b2b3bda
Naelle47 <benard.audrey1@gmail.com>
2025-07-16 16:59:32 +0200

    ajout des data annotations dans les modèles : - Jeu - Catégorie - Theme - Utilisateur

--- ProjetFilRouge/Models/Jeu.cs
+++ ProjetFilRouge/Models/Jeu.cs
@@ -1,13 +1,25 @@
-﻿namespace ProjetFilRouge.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetFilRouge.Models
 {
     public class Jeu
     {
+
         public int JeuId { get; set; }
+
+        [Required(ErrorMessage ="Le champ est obligatoire.")]
         public string? Titre { get; set; }
+
+        [Required(ErrorMessage ="Le champ est obligatoire.")]
+        [StringLength(120,ErrorMessage ="La description ne doit pas dépasser 120 caractères.")]
         public string? Description { get; set; }
+
         public string? Image { get; set; }
         public int NombreJoueursRecommandes { get; set; }
         public int TempsJeuMoyen { get; set; }
+
+        [Display(Name ="Ajouté le : ")]
+        [DataType(DataType.Date)]
         public DateTime DateAjout { get; set; }
 
         // relations 
--- ProjetFilRouge/Models/Utilisateur.cs
+++ ProjetFilRouge/Models/Utilisateur.cs
@@ -1,12 +1,26 @@
-﻿namespace ProjetFilRouge.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetFilRouge.Models
 {
     public class Utilisateur
     {
         public int UtilisateurId { get; set; }
+
+        [Required(ErrorMessage ="Le champ est obligatoire.")]
         public string? Nom { get; set; }
+
+        [Required(ErrorMessage ="Le champ est obligatoire.")]
+        [EmailAddress(ErrorMessage ="Adresse e-mail invalide.")]
         public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Le champ est obligatoire.")]
         public string? MotDePasse { get; set; }
+
+        [Display(Name = "Inscription : ")]
+        [DataType(DataType.Date)]
         public DateTime DateInscription { get; set; }
+
         public bool Admin { get; set; }
     }
 }
