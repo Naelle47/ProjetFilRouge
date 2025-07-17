535e0e5e66cf84a16b8d269484899696ee73eb0e
Naelle47 <benard.audrey1@gmail.com>
2025-07-17 08:26:23 +0200

    ajustement de la longueur de la chaîne de caractère pour le modèle catégorie

--- ProjetFilRouge/Models/Categorie.cs
+++ ProjetFilRouge/Models/Categorie.cs
@@ -7,7 +7,7 @@ namespace ProjetFilRouge.Models
         public int CategorieId { get; set; }
 
         [Required(ErrorMessage ="Le champ est obligatoire.")]
-        [StringLength(50,ErrorMessage ="Le nom ne doit pas dépasser 50 caractères.")]
+        [StringLength(90,ErrorMessage ="Le nom ne doit pas dépasser 90 caractères.")]
         public string? Nom { get; set; }
 
         [StringLength(120,ErrorMessage ="La description ne doit pas dépasser 120 caractères.")]
