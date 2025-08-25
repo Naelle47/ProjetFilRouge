572948089c3735935081581fcd2ce74c4e693c9a
Naelle47 <benard.audrey1@gmail.com>
2025-08-25 11:53:06 +0200

    Dernières modifs - ajout du lien dans les menus du header et du footer pour accéder au catalogue de jeux - réfaction du code pour le catalogue et la vue détail (partie backend) - ajout de la section formulaire pour la soumission d'un nouveau jeu (pas finie) - ajout d'une section cta pour participer aux événéments

--- ProjetFilRouge/Controllers/JeuxController.cs
+++ ProjetFilRouge/Controllers/JeuxController.cs
@@ -28,7 +28,17 @@ namespace ProjetFilRouge.Controllers
         // afficher le catalogue des jeux
         public IActionResult Index()
         {
-            string query = "Select * from jeux";
+            string query = @"
+            SELECT 
+                jeuid_pk AS ""JeuId"", 
+                Titre, 
+                Description, 
+                Image, 
+                NombreJoueursRecommandes, 
+                TempsJeuMoyen, 
+                DateAjout
+            FROM Jeux
+            ORDER BY Titre";
             List<Jeu> jeux;
             using (var connexion = new NpgsqlConnection(_connexionString))
             {
@@ -48,20 +58,68 @@ namespace ProjetFilRouge.Controllers
             // déclaration de la variable permettant de contenir le résultat de la requête SQL
             // exécuter la requête SQL
             // retourner la vue contenant le résultat
-            string query = "SELECT * from Jeux WHERE id=@identifiant";
-            Jeu jeux;
+            string query = @"
+            SELECT 
+                jeuid_pk AS ""JeuId"", 
+                Titre, 
+                Description, 
+                Image, 
+                NombreJoueursRecommandes, 
+                TempsJeuMoyen, 
+                DateAjout
+            FROM Jeux
+            WHERE jeuid_pk = @identifiant";
+            Jeu jeu;
             using (var connexion = new NpgsqlConnection(_connexionString))
             {
                 try
                 {
-                    jeux = connexion.QuerySingle<Jeu>(query, new {identifiant = id}); // {identifiant = id} -> objet anonyme entre accolades.
+                    jeu = connexion.QuerySingle<Jeu>(query, new {identifiant = id}); // {identifiant = id} -> objet anonyme entre accolades.
                 }
                 catch (SystemException)
                 {
                     return NotFound();
                 }
             }
-            return View(jeux);
+            return View(jeu);
+        }
+
+        [HttpPost]
+        public IActionResult Create(EditeurJeuxViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Si le formulaire n'est pas valide, recharge la liste des jeux
+                string query = @"
+            SELECT 
+                jeuid_pk AS ""JeuId"",
+                Titre,
+                Description,
+                Image,
+                NombreJoueursRecommandes,
+                TempsJeuMoyen,
+                DateAjout
+            FROM Jeux
+            ORDER BY Titre";
+
+                using (var connexion = new NpgsqlConnection(_connexionString))
+                {
+                    model.Jeux = connexion.Query<Jeu>(query).ToList();
+                }
+
+                return View("Index", model);
+            }
+
+            string insertQuery = @"
+        INSERT INTO Jeux (Titre, TempsJeuMoyen, NombreJoueursRecommandes, Categorie, DateSortie)
+        VALUES (@Titre, @TempsJeuMoyen, @NombreJoueursRecommandes, @Categorie, @DateSortie)";
+
+            using (var connexion = new NpgsqlConnection(_connexionString))
+            {
+                connexion.Execute(insertQuery, model);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
--- ProjetFilRouge/ViewModel/EditeurJeuxViewModel.cs
+++ ProjetFilRouge/ViewModel/EditeurJeuxViewModel.cs
@@ -1,8 +1,32 @@
-﻿
-namespace ProjetFilRouge.ViewModel
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetFilRouge.Models
 {
     public class EditeurJeuxViewModel
     {
+        // Champs pour le formulaire d'ajout
+        [Required(ErrorMessage = "Le titre est obligatoire.")]
+        public required string Titre { get; set; }
 
+        [Required(ErrorMessage = "Le temps de jeu est obligatoire.")]
+        [Display(Name = "Temps de jeu (min)")]
+        public int TempsJeuMoyen { get; set; }
+
+        [Required(ErrorMessage = "Le nombre de joueurs est obligatoire.")]
+        [Display(Name = "Nombre de joueurs")]
+        public int NombreJoueursRecommandes { get; set; }
+
+        [Required(ErrorMessage = "La catégorie est obligatoire.")]
+        public required string Categorie { get; set; }
+
+        [Required(ErrorMessage = "La date de sortie est obligatoire.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date de sortie")]
+        public DateTime DateSortie { get; set; }
+
+        // Liste des jeux pour la galerie
+        public List<Jeu> Jeux { get; set; } = new List<Jeu>();
     }
 }
