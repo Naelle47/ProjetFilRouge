09c5908e3f4203ac0f050776efe7764c41a2932c
Naelle47 <benard.audrey1@gmail.com>
2025-08-28 11:39:52 +0200

    - modifs formulaire pour modifications/suppression de jeux toujours en cours - ajout de la gestion des cookies en lien avec le RGPD

--- ProjetFilRouge/Program.cs
+++ ProjetFilRouge/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential 
+    // cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+
+    options.MinimumSameSitePolicy = SameSiteMode.Lax;
+    options.ConsentCookieValue = "true";
+});
 
 var app = builder.Build();
 
@@ -16,6 +25,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseCookiePolicy();
 
 app.MapControllerRoute(
     name: "default",
--- ProjetFilRouge/ViewModel/EditeurJeuxViewModel.cs
+++ ProjetFilRouge/ViewModel/EditeurJeuxViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ProjetFilRouge.Models
 {
     public class EditeurJeuxViewModel
     {
-       
+       public Jeu? jeu {  get; set; }
+        public List<SelectListItem> categories { get; set; } = new List<SelectListItem>();
+        public required string action { get; init; }
+        public required string titre { get; init; }
+
+        public int? JeuId { get; set; } = null;
     }
 }
