67996b21d100f99d5f1ee8a767a755c2ed1e9a94
Naelle47 <benard.audrey1@gmail.com>
2025-09-22 10:27:43 +0200

    -suppression d'espaces

--- ProjetFilRouge/Models/Jeu.cs
+++ ProjetFilRouge/Models/Jeu.cs
@@ -4,26 +4,19 @@ namespace ProjetFilRouge.Models
     public class Jeu
     {
         public int JeuId { get; set; }
-
         [Required(ErrorMessage ="Le champ est obligatoire.")]
         public string? Titre { get; set; }
-
         [Required(ErrorMessage ="Le champ est obligatoire.")]
         [StringLength(120,ErrorMessage ="La description ne doit pas dépasser 120 caractères.")]
         public string? Description { get; set; }
-
         public string? Image { get; set; }
-
         [Display(Name ="Joueurs")]
         public int NombreJoueursRecommandes { get; set; }
-
         [Display(Name ="Durée")]
         public int TempsJeuMoyen { get; set; }
-
         [Display(Name ="Ajouté le : ")]
         [DataType(DataType.Date)]
         public DateTime DateAjout { get; set; }
-
         public required IFormFile ImageFile { get; set; }
 
         // relations
